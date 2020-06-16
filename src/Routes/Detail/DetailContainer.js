ceb353efa465de0c1a0bb0754b62cdabfd178a6c
leejieun94 <xaq03061@gmail.com>
2020-06-16 15:05:56 +0900

    #5.5 Detail Container part One
    header 컴포넌트는 우리의 라우터의 위치를 알고 있음. 왜냐하면 withRouter 컴포넌트를 사용했기 때문.
    
    알아야하는 것 :
    1) 우리가 /movie /show 어디에 있는지 알아내기.(둘다 같은 페이지, 같은 컴포넌트로 감)
    2) 어떤 숫자가 1) movie/ show/ 있는지 볼 것임. props를 봄. 우리의 props는 match라는걸 가짐. 여기에 params : {id: "121212"}.
    
    수행내용 : id를 받아서, parseInt를 사용해서 리소스들을 string 요청에 낭비 막음.
    
    에러 : state 오타로 25줄에서 null 값을 입력받음.

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -2,11 +2,24 @@ import React from "react";
 import DetailPresenter from "./DetailPresenter";
 
 export default class extends React.Component {
-  staet = {
+  state = {
     result: null,
     error: null,
     loading: true,
   };
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+  }
 
   render() {
     const { result, error, loading } = this.state;
